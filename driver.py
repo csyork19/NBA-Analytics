7bb9da3fded9820c1c676a6303eb0c1f8fc46b49
Christopher York <csyork19@gmail.com>
2021-10-17 17:24:46 -0400

    Code cleanup for driver.py

--- driver.py
+++ driver.py
@@ -3,14 +3,7 @@ import pandas as pd
 
 lines = []
 
-def addTeamsToList():
-    with open('files/nba_teams_abbreviations.txt','r') as file:
-        count = 0
-        for line in file:
-            line = line.strip()
-            lines.append(line)
-
-def getListOfTeams():
+def get_list_of_teams():
     print("NBA Teams")
     with open('files/nba_teams.txt','r') as file:
         count = -1
@@ -18,13 +11,13 @@ def getListOfTeams():
             count +=1
             print(str(count) + ". " + line.strip())
 
-def getListOfTeamAbbrv():
+def get_list_of_team_abbrv():
     with open('files/nba_teams_abbreviations.txt','r') as file:
         count = 0
         for line in file:
             count +=1
 
-def getAbbreviationForUserNbaTeam(nba_team):
+def get_abbreviation_for_user_nba_team(nba_team):
     abbr_line_count = 0
     with open('files/nba_teams.txt','r') as file:
         count = 0
@@ -40,7 +33,7 @@ def getAbbreviationForUserNbaTeam(nba_team):
                 return str(line)
             count +=1
 
-def getTeamRoster(selected_team):
+def get_team_roster(selected_team):
     team = selected_team
     url = 'https://www.basketball-reference.com/teams/'+str(team)+'/2022.html'
     response = requests.get(url)
@@ -50,14 +43,14 @@ def getTeamRoster(selected_team):
 
 def mainMenu():
     # Prompt user to select from the list of teams
-    getListOfTeams()
+    get_list_of_teams()
     user_nba_team = input("Please select a team from the list above: \n")
 
     # Get the abbriviation for the nba team
-    getListOfTeamAbbrv()
-    teams = getAbbreviationForUserNbaTeam(str(user_nba_team))
+    get_list_of_team_abbrv()
+    teams = get_abbreviation_for_user_nba_team(str(user_nba_team))
 
     # Print team roster
-    getTeamRoster("".join(teams.split()))
+    get_team_roster("".join(teams.split()))
 
 mainMenu()
