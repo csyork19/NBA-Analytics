2cf74a18ecec89ca3f8928c73e5021c71ea18800
Christopher York <csyork19@gmail.com>
2021-10-17 15:20:22 -0400

    Update to get players career and season stats

--- player_stats.py
+++ player_stats.py
@@ -24,59 +24,73 @@ results1 = soup.find_all("div", attrs={'class':'p2'})
 
 # PER, WS
 results2 = soup.find_all("div", attrs={'class':'p3'})
+count = 0
+
+def get_dictionary_of_player_stats(category,complete_player_stats,dict):
+    print(dict)
+    print(complete_player_stats)
+    print(category)
+
+    index = 0
+    for i in category:
+        if index < 2:
+            for j in complete_player_stats[0:1]:
+                print ("inside loop")
+                print("index is " + str(index))
+                value = complete_player_stats[index]
+                dict[i] = dict.get(i,[]).append(value)
+                index += 1
 
 
+    print(dict)
+
+    #for i in category, complete_player_stats:
+        #i = i.strip()
+        #index = 0
+        #if index < 2:
+            #dict[i] = complete_player_stats[i]
 
 for stats in results:
     #player_stats = ' '.join(stats.text.split())
     #print(player_stats)
     list = []
     player_stats = []
+    dict = {}
+    season_stats = []
+    career_stats = []
     #player_stats = stats.get_text().strip()
     divs = stats.find_all('strong')
     stat = stats.find_all('p')
 
     for item in divs:
         list.append(item.text.strip())
+        item = item.text.strip()
+        dict[item] = None
 
-    for item in stat:
-        player_stats.append(item.text.strip())
     #print("Size of "+ str(len(stats)))
-    print(list)
-    print(player_stats)
+    for item in stat:
 
-for stats1 in results1:
-    #player_stats = ' '.join(stats.text.split())
+        print("The index is " + str(count))
+
+        if count % 2 == 0:
+            print(item.text.strip())
+            season_stats.append(item.text.strip())
+            count += 1
+            print ("The index count is even " + str(count))
+        else:
+            print ("The index count is odd " + str(count))
+            career_stats.append(item.text.strip())
+            count += 1
+        #player_stats.append(item.text.strip())
+
+
+    #print(list)
     #print(player_stats)
-    list = []
-    player_stats = []
-    #player_stats = stats.get_text().strip()
-    divs = stats1.find_all('strong')
-    stat = stats1.find_all('p')
+    #print(dict)
 
-    for item in divs:
-        list.append(item.text.strip())
+    #get_dictionary_of_player_stats(list,player_stats,dict)
 
-    for item in stat:
-        player_stats.append(item.text.strip())
-    #print("Size of "+ str(len(stats)))
-    print(list)
-    print(player_stats)
-
-for stats2 in results2:
-    #player_stats = ' '.join(stats.text.split())
-    #print(player_stats)
-    list = []
-    player_stats = []
-    #player_stats = stats.get_text().strip()
-    divs = stats2.find_all('strong')
-    stat = stats2.find_all('p')
-
-    for item in divs:
-        list.append(item.text.strip())
-
-    for item in stat:
-        player_stats.append(item.text.strip())
-    #print("Size of "+ str(len(stats)))
-    print(list)
-    print(player_stats)
+    print ("The season stats are")
+    print(season_stats)
+    print("The career stats are")
+    print(career_stats)
